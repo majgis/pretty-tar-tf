b6d4a2a5569494776e258eb30da4741d1acfc7c2
Michael Jackson <majgis@gmail.com>
2016-02-27 20:45:32 -0700

    Fixed issue when no node_modules present.

--- bin/pretty-tar-tf.js
+++ bin/pretty-tar-tf.js
@@ -16,37 +16,37 @@ var otherFiles = {}
 
 
 stdin.on('data', function onChunk (chunk) {
-	var split = chunk.toString('utf8').split(eol)
-	for (var i=0; i < split.length; i++){
-		var line = split[i]
-		if (line){
+  var split = chunk.toString('utf8').split(eol)
+  for (var i = 0; i < split.length; i++) {
+    var line = split[i]
+    if (line) {
       line = line.replace(/^package\//, '')
       var lineSplit = line.split(sep)
-			if (lineSplit[0] === 'node_modules'){
-				packageNames[lineSplit[1]] = null
-			} else {
+      if (lineSplit[0] === 'node_modules') {
+        packageNames[lineSplit[1]] = null
+      } else {
         var fileName = lineSplit.pop()
         var folderPath = lineSplit.join(sep)
         otherFiles[folderPath] = otherFiles[folderPath] || []
         otherFiles[folderPath].push(fileName)
-			}
-		}
-	}
+      }
+    }
+  }
 })
 
 stdin.on('end', function onEnd () {
   console.log()
   var keys = []
   var key
-  for (key in otherFiles){
+  for (key in otherFiles) {
     keys.push(key)
   }
   keys.sort()
   var indent = ''
-  for (var keyIndex = 0; keyIndex < keys.length; keyIndex++){
+  for (var keyIndex = 0; keyIndex < keys.length; keyIndex++) {
     key = keys[keyIndex]
     var indent
-    if (key){
+    if (key) {
       indent = '  '
       console.log()
       console.log(key + sep)
@@ -55,13 +55,19 @@ stdin.on('end', function onEnd () {
     }
 
     var files = otherFiles[key]
-    for (var filesIndex = 0; filesIndex < files.length; filesIndex++){
+    for (var filesIndex = 0; filesIndex < files.length; filesIndex++) {
       console.log(indent + files[filesIndex])
     }
   }
 
-	console.log('\n\nnode_modules/')
-	for (var key in packageNames){
-		console.log('  ' + key)
-	}
+  var modules = []
+  for (var key in packageNames) {
+    modules.push('  ' + key)
+  }
+  if (modules.length) {
+    console.log('\n\nnode_modules/')
+    for (var moduleIndex = 0; moduleIndex < modules.length; moduleIndex++) {
+      console.log(modules[moduleIndex])
+    }
+  }
 })
